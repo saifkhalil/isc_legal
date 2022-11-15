f44b966cea12ab05ff06a4c71e1d3c828cd8b22d
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2022-11-15 10:21:54 +0300

    intial commit

--- cases/views.py
+++ cases/views.py
@@ -111,7 +111,7 @@ class LitigationCasesViewSet(viewsets.ModelViewSet):
         #   FullWordSearchFilter,
           ]
     # perm_slug = "cases.LitigationCases"
-    filterset_fields = ['id', 'name']
+    filterset_fields = ['id', 'Stage','case_type','case_category','assignee','court']
     # word_fields = ('name','description')
     search_fields = ['@name','@description']
     ordering_fields = ['created_at', 'id','modified_at']
