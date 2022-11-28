9fafc6bb664cb31caad6848794c7d76dbfc2667d
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2022-11-28 13:38:40 +0300

    get only my records if not manager

--- accounts/models.py
+++ accounts/models.py
@@ -135,6 +135,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return str(self.username)
 
+    def full_name(self):
+        return str(self.firstname + " " + self.lastname)
+
     # For checking permissions. to keep it simple all admin have ALL permissons
 
     # Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
